e61035a61d0123fc5e1f2e47962c51fc908c171e
Isaac Gorman <emailme@isaacg.net>
2021-06-18 18:54:13 -0500

    refactored rps to use all functions

--- lesson_2/rock_paper_scissors/walkThrough.js
+++ lesson_2/rock_paper_scissors/walkThrough.js
@@ -1,32 +1,26 @@
-/*
-    Process Problem
-        Objects
-          rock
-          scissors
-          paper
-        Rules
-          rock vs scissors = rock wins
-          paper vs rock = paper wins
-          scissors vs paper = scissors wins
-          same object vs same object = tie
-
-        Game Flow
-          1. User picks an object
-          2. Computer picks an object
-          3. Winner is displayed or tie
-
-    Examples
-
-    Data Structure
-
-    Algorithm
-
-    Code
-*/
-
 const READ_LINE = require("readline-sync");
 const VALID_CHOICES = ["rock", "paper", "scissors"];
-let playAgain = true;
+
+function prompt(message) {
+  console.log(`➡️  ${message}`);
+}
+
+function getUserChoice() {
+  prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
+  let userChoice = READ_LINE.question();
+
+  while (!VALID_CHOICES.includes(userChoice)) {
+    prompt("That's not a valid choice");
+    userChoice = READ_LINE.question();
+  }
+  return userChoice;
+}
+
+function getComputerChoice() {
+  let randomIndex = Math.round(Math.random() * (VALID_CHOICES.length - 1));
+  let computerChoice = VALID_CHOICES[randomIndex];
+  return computerChoice;
+}
 
 function displayWinner(userChoice, computerChoice) {
   let computerWinsMessage = `Computer win's! \n${computerChoice} beats ${userChoice}`;
@@ -51,44 +45,31 @@ function displayWinner(userChoice, computerChoice) {
   if (computerChoice === userChoice) prompt("Tie!");
 }
 
-function validateUserChoice(userChoice) {
-  while (!VALID_CHOICES.includes(userChoice)) {
-    prompt("That's not a valid choice");
-    userChoice = READ_LINE.question();
-  }
-}
+function askToPlayAgain() {
+  prompt("Do you want to play again? (y/n): ");
+  let answer = READ_LINE.question().trim().toLowerCase();
 
-function getComputerChoice() {
-  let randomIndex = Math.round(Math.random() * (VALID_CHOICES.length - 1));
-  let computerChoice = VALID_CHOICES[randomIndex];
-  return computerChoice;
-}
-
-function validatePlayAgain(answer) {
   while (answer[0] !== "n" && answer[0] !== "y") {
     prompt('Please enter "y" or "n" .');
     answer = READ_LINE.question().trim().toLowerCase();
   }
+
+  return answer;
 }
 
-function prompt(message) {
-  console.log(`➡️  ${message}`);
+function playRockPaperScissors() {
+  while (true) {
+    let userChoice = getUserChoice();
+    let computerChoice = getComputerChoice();
+
+    prompt(`You chose 😊 ${userChoice}, 🤖 computer chose ${computerChoice}`);
+
+    displayWinner(userChoice, computerChoice);
+
+    let answer = askToPlayAgain();
+
+    if (answer[0] !== "y") break;
+  }
 }
 
-while (playAgain) {
-  prompt(`Choose one: ${VALID_CHOICES.join(", ")}`);
-  let userChoice = READ_LINE.question();
-  validateUserChoice(userChoice);
-
-  let computerChoice = getComputerChoice();
-
-  prompt(`You chose 😊 ${userChoice}, 🤖 computer chose ${computerChoice}`);
-
-  displayWinner(userChoice, computerChoice);
-
-  prompt("Do you want to play again? (y/n): ");
-  let answer = READ_LINE.question().trim().toLowerCase();
-  validatePlayAgain(answer);
-
-  if (answer[0] !== "y") playAgain = false; // or simply write a break; when while condition is set to (true)
-}
+playRockPaperScissors();
