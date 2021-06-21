f6e5dd021552737297c0c4ca5785037284f754e1
Isaac Gorman <emailme@isaacg.net>
2021-06-21 15:01:54 -0500

    refactored file structure

--- lesson_2/Errors/referenceErrors.js
+++
@@ -1,7 +0,0 @@
-// Q: When do reference errors occur?
-// A: A ReferenceError happens when a variable ...
-// ... or function is invoked without it actually existing
-// ... reference errors occur when a non-existing values are called
-
-// a; // ReferenceError: a is not defined
-a(); // ReferenceError: a is not defined
