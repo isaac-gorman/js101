05b746ea5d751bef4f6fb78623e4e32091152f00
Isaac Gorman <emailme@isaacg.net>
2021-06-23 22:59:21 -0500

    used a forEach method instead of a for loop

--- lesson_3/1_easy_practice_problems/q3.js
+++ lesson_3/1_easy_practice_problems/q3.js
@@ -12,18 +12,24 @@ let getEntry = (entry, object) => {
   let arrayOfKeys = Object.keys(object);
   let containsEntry = false;
 
-  for (let index = 0; index < arrayOfKeys.length; index++) {
-    if (arrayOfKeys[index] === entry) {
+  //   for (let index = 0; index < arrayOfKeys.length; index++) {
+  //     if (arrayOfKeys[index] === entry) {
+  //       containsEntry = true;
+  //     }
+  //   }
+
+  arrayOfKeys.forEach((crrV) => {
+    if (crrV === entry) {
       containsEntry = true;
     }
-  }
+  });
 
   console.log(containsEntry);
 };
-getEntry("Spot", ages);
+getEntry("Ari", ages);
 
 // The pre-built method way
 let getEntryEasy = (entry, object) => {
   console.log(object.hasOwnProperty(entry));
 };
-getEntryEasy("Isaac", ages);
+// getEntryEasy("Isaac", ages);
