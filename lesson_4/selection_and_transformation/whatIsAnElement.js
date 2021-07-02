52536daef75881f49a43ac30d6abcdb4fdca0bc5
Isaac Gorman <emailme@isaacg.net>
2021-07-02 16:24:58 -0500

    ⚡collections: more array and object methods

---
+++ lesson_4/selection_and_transformation/whatIsAnElement.js
@@ -0,0 +1,16 @@
+/*  What is an Element?
+
+    Q: Do arrays have property names? 
+    A:
+
+    Array Keys
+
+    Q: 
+    A:
+
+*/
+
+let array = [];
+console.log(array);
+console.log(array.length);
+console.log(Object.keys(array));
