33aca61ef9e5dd1518313de0ea4fbb6b90df3d9d
Isaac Gorman <emailme@isaacg.net>
2021-07-05 14:51:40 -0500

    ⚡saving prog

---
+++ lesson_5/practice_problems/p6.js
@@ -0,0 +1,25 @@
+// Coding Challenge:
+// - One of the most frequently used real-world string operations is that of "string substitution," where we take a hard-coded string and modify it with various parameters from our program.
+
+// - Given this previously seen family object, print the name, age, and gender of each family member:
+
+let munsters = {
+  herman: { age: 32, gender: "male" },
+  lily: { age: 30, gender: "female" },
+  grandpa: { age: 402, gender: "male" },
+  eddie: { age: 10, gender: "male" },
+  marilyn: { age: 23, gender: "female" },
+};
+
+let munstersMembers = Object.keys(munsters);
+let munstersFamilyIntro = () => {
+  return munstersMembers.forEach((crrV) => {
+    let age = crrV["age"];
+    console.log(crrV.age);
+    let upperCaseName = crrV.charAt(0).toUpperCase() + crrV.slice(1);
+    let gender = crrV["gender"];
+    console.log(`${upperCaseName} is  ${age}-year-old ${gender}`);
+  });
+};
+
+munstersFamilyIntro();
