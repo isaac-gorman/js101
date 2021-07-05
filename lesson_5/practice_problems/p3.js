709f3cf3dda8799939a276ef04b080324258d520
Isaac Gorman <emailme@isaacg.net>
2021-07-05 13:27:42 -0500

    ⚡saving prog

---
+++ lesson_5/practice_problems/p3.js
@@ -0,0 +1,10 @@
+// For each of these collection objects, demonstrate how you would access the letter g.
+
+let arr1 = ["a", "b", ["c", ["d", "e", "f", "g"]]];
+let flatArray = arr1.flat(Infinity);
+// console.log(flatArray[flatArray.indexOf("g")]);
+
+let arr2 = [
+  { first: ["a", "b", "c"], second: ["d", "e", "f"] },
+  { third: ["g", "h", "i"] },
+];
