52dc3595a8a8f1f7ca64b4440e06f4e6e9811670
Isaac Gorman <emailme@isaacg.net>
2021-07-05 14:15:53 -0500

    ⚡saving prog

--- lesson_5/practice_problems/p4.js
+++ lesson_5/practice_problems/p4.js
@@ -3,5 +3,10 @@
 
 let arr1 = [1, [2, 3], 4];
 // console.log(arr1);
-arr1[1][1] = 4;
+// arr1[1][1] = 4;
 // console.log(arr1);
+
+let arr2 = [{ a: 1 }, { b: 2, c: [7, 6, 5], d: 4 }, 3];
+// console.log(arr2);
+arr2[2] = 4;
+// console.log(arr2);
