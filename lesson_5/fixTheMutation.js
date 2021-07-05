67cf277d97a2297da1c07d77b71fb47716e45cc1
Isaac Gorman <emailme@isaacg.net>
2021-07-05 13:09:43 -0500

    ⚡saving prog

--- lesson_5/fixTheMutation.js
+++ lesson_5/fixTheMutation.js
@@ -1,5 +1,12 @@
 let arr = [1, 2];
 
-arr.map((subArr) => arr.pop());
+// issue code
+// arr.map((subArr) => arr.pop());// => [ 2, <1 empty item> ]
 
-console.log(arr); // => [ 2, <1 empty item> ]
+// solution code
+let newArray = arr.filter((num, index, array) => {
+  return index !== array.length - 1;
+});
+
+console.log("original array: ", arr);
+console.log("new array: ", newArray);
