8b6c0050e0c98e7c6dc7e0e1b20d7c865bcbb5c8
Isaac Gorman <emailme@isaacg.net>
2021-07-06 08:51:16 -0500

    ⚡saving prog

---
+++ lesson_5/practice_problems/p12.js
@@ -0,0 +1,36 @@
+// Challenge:
+// - Given the following data structure, use a combination of methods, including filter, to return a new array identical in structure to the original, but containing only the numbers that are multiples of 3.
+
+let arr = [[2], [3, 5, 7], [9], [11, 15, 18]];
+
+// Clarification:
+// - How would I evaluate what numbers are multiples of three?
+// console.log(18 % 3); // => 0
+
+// I/O
+// Input:
+// -> [[2], [3, 5, 7], [9], [11, 15, 18]]
+
+// Process:
+// - access the sub-arrays of the arr
+// - access each element of the sub-array
+// - evaluate if the element is a multiple of 3.
+// - if a element is a multiple of 3 push it to a new array
+// - return the new array
+
+// Output:
+// new array -> [[3], [9], [15, 18]]
+
+let onlyMultiplesOf = (integer, array) => {
+  let newArray = array.map((subArray) => {
+    let arrayOfMultiples = subArray.filter((number) => {
+      return number % integer === 0;
+    });
+
+    return arrayOfMultiples;
+  });
+  return newArray;
+};
+
+// console.log(onlyMultiplesOf(3, arr));
+// console.log(onlyMultiplesOf(2, arr));
