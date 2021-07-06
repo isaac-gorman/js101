c881084284ae18600269ab94b1766143868f340d
Isaac Gorman <emailme@isaacg.net>
2021-07-06 18:24:19 -0500

    ⚡saving prog

--- lesson_5/practice_problems/p17.js
+++ lesson_5/practice_problems/p17.js
@@ -17,46 +17,24 @@ let generateUUID = () => {
   return UUID_Code;
 };
 
-// let sectionString = "";
-// sectionString += "a";
-// sectionString += 7;
+let generateRandomSection = (numOfCharacter) => {
+  let sectionString = "";
 
-// console.log(sectionString);
-// // generate a random choice for either a string or number
-// let randomChoice = Math.floor(Math.random() * (1 - 0 + 1) + 0);
+  while (sectionString.length < numOfCharacter) {
+    sectionString.trim();
 
-// if (randomChoice === 0) {
-//   // generate a random letter
-//   let randomChar = randomLetter();
-//   // then push that letter to the section string
-//   sectionString += randomChar;
-// } else {
-//   let randomInt = randomNumber();
-//   sectionString += randomInt;
-// }
+    let randomChoice = Math.floor(Math.random() * (1 - 0 + 1) + 0);
 
-// console.log("sectionString: ", sectionString);
-
-// return;
-// Build and push section 1 of 5
-
-let sectionString = "";
-console.log("sectionString: ", sectionString);
-
-while (sectionString.length < 8) {
-  sectionString.trim();
-  // generate a random choice for either a string or number
-  let randomChoice = Math.floor(Math.random() * (1 - 0 + 1) + 0);
-
-  if (randomChoice === 0) {
-    // generate a random letter
-    let randomChar = randomLetter();
-    // then push that letter to the section string
-    sectionString += randomChar;
-  } else {
-    let randomInt = randomNumber();
-    sectionString += randomInt;
+    if (randomChoice === 0) {
+      let randomChar = randomLetter();
+      sectionString += randomChar;
+    } else {
+      let randomInt = randomNumber();
+      sectionString += randomInt;
+    }
   }
-}
 
-console.log("sectionString: ", sectionString);
+  return sectionString;
+};
+
+console.log(generateRandomSection(8));
