ac8744e3c767ba25c506031d035d7802081df151
Isaac Gorman <emailme@isaacg.net>
2021-07-06 10:54:56 -0500

    ⚡saving prog

--- lesson_5/practice_problems/p16.js
+++ lesson_5/practice_problems/p16.js
@@ -10,13 +10,13 @@ let arr = [
 ];
 
 // Process:
-// - deep copy the arr value
-// - create a blank object named extractedData {}
-// - iterate over each sub-array in the element
-// - extract the sub-array values and assign them to the extractedData {} object
-// - - extractedData[subArr[0]] = subArr[1]
-// - repeat the above two steps for the entire length of the outer array
-// - return extractedData {}
+// [x] deep copy the arr value
+// [x] create a blank object named extractedData {}
+// [x] iterate over each sub-array in the element
+// [x] extract the sub-array values and assign them to the extractedData {} object
+// [x] - extractedData[subArr[0]] = subArr[1]
+// [x] repeat the above two steps for the entire length of the outer array
+// [x] return extractedData {}
 
 // Output:
 /*
@@ -27,3 +27,12 @@ let arr = [
         D: ["a", "b", "c"]  
     }
 */
+
+let deepCopy = JSON.parse(JSON.stringify(arr));
+let extractedData = {};
+
+deepCopy.forEach((subArray) => {
+  extractedData[subArray[0]] = subArray[1];
+});
+
+console.log(extractedData);
