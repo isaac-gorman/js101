b407418238599b00d0e792a4394e85b8b68153fe
Isaac Gorman <emailme@isaacg.net>
2021-07-06 11:34:52 -0500

    ⚡saving prog

--- lesson_5/practice_problems/p17.js
+++ lesson_5/practice_problems/p17.js
@@ -4,3 +4,12 @@
 // - Each UUID consists of 32 hexadecimal characters (the digits 0-9 and the letters a-f) represented as a string. The value is typically broken into 5 sections in an 8-4-4-4-12 pattern, e.g., 'f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91'.
 
 // - Write a function that takes no arguments and returns a string that contains a UUID.
+
+// Playing UTF-16 code system:
+// Intentional Process:
+// - Input:
+// -> random number within the range for UTF - 16 code of "a - f"
+// - Output:
+// -> lowercase letter "a - f" of the english alphabet
+
+console.log(fromCharCode(61));
