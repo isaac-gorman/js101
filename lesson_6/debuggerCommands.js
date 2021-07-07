af7a62a5d3df21ea08f3efe91e3a3c198f12bc67
Isaac Gorman <emailme@isaacg.net>
2021-07-07 09:21:47 -0500

    ⚡saving prog

---
+++ lesson_6/debuggerCommands.js
@@ -0,0 +1,33 @@
+// Annotations
+/* 
+    Q: What will I cover in this assignment?
+    
+   Executing Expression Functions: 
+    Q: What is the exec command?
+
+    Q: What do I need in order to execute a function with the exec key word?
+
+    Q: What is another way to evaluate expressions or execute functions?
+
+   Setting and Clearing Breakpoints:
+    Q: How have I learned to set up breakpoint in my code so far?
+
+    Q: How can I add or remove breakpoint as it is running within the debugger?
+
+    Q: What is setBreakpoint() / sb()?
+
+    Q: What is clearBreakpoint() / cb()?
+
+   Stepping Into Functions:
+    Q: How would I step through function as of now?
+
+    Q: When reaching the inner function invocation how would I move into the execution of that function, rather than just moving over to the next expression? Why is this useful?
+
+   Other Commands:
+    Q: What are some other useful commands when trouble shooting code?
+
+    Q: What does the help() command do?
+
+    Q: What does the list() command do?
+
+*/
