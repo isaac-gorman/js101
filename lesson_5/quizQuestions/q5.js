5b960cd69166e8b2550118583d086993ae014e9f
Isaac Gorman <emailme@isaacg.net>
2021-07-07 08:23:50 -0500

    ⚡saving prog

--- lesson_5/quizQuestions/q5.js
+++ lesson_5/quizQuestions/q5.js
@@ -1,34 +1,32 @@
+// First-Class Functions vs Higher-Order Functions
 /*
     Q: So is there a difference between higher order functions and first-class functions?
 
     - A: Yes, there is a difference. 
+*/
 
-      First-Class Functions: 
+// - First-Class Functions
+/*
+    First-Class Functions: 
+    
       Whenever a language has first-class functions, it means that the language treats functions as values - that you can assign a function into a variable, pass it around etc. 
 
-      Example: 
-      let hi = function () {
-          return "hello";
-      };
+*/
 
-      console.log(hi()); => "hello"
+// this is an example of first class function because I can assign the anonymous function to just about any value
+let hi = function () {
+  return "hello";
+};
 
-      Higher-Order Functions:
+console.log(hi()); // => "hello"
+
+// - Higher-Order Functions
+/*
+    Higher-Order Functions:
+
       Higher-Order Functions are functions that are work with other functions, meaning that they take one or more functions as arguments and can also return a functions:
 
       Example: 
-
-      
-
-
-
-
-
-
-};
-
-
-
 */
 
 // regular function
@@ -55,3 +53,6 @@ function invoker() {
 }
 
 console.log(invoker());
+
+// Symbiosis
+// - It would be hard to imagine one with out the other. Both give each other the functionality to preform there task.Higher order functions support first-class functions and conversely.
