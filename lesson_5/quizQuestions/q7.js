9f976adb36b6f419b40875251144089256719aa5
Isaac Gorman <emailme@isaacg.net>
2021-07-07 08:55:11 -0500

    ⚡saving prog

--- lesson_5/quizQuestions/q7.js
+++ lesson_5/quizQuestions/q7.js
@@ -6,18 +6,20 @@ function evenValues(array) {
     // array[0]: value = 1
     // array[1]: value = 4
     // array[2]: value = 4
+    // array[3]: value = 5
+    // array[4]: value = 9
+    // array[5]: value = 12
 
     if (value % 2 === 0) {
       evens.push(value);
     }
     array.shift();
-    //   removed value = 1
   });
 
-  // evens = [4, 6, 12]
+  // evens = [4, 4, 12]
   return evens;
 }
-// live = [3, 4, 2, 4, 6, 5, 7, 9, 10, 12]
+// live = [ 6, "5", 7, 9, 10, 12]
 // og = [1, 3, 4, 2, 4, 6, 5, 7, 9, 10, 12]
 //
 //
