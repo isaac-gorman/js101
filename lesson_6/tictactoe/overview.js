324c758dbf520099a18d0a9b14569b22453e4fad
Isaac Gorman <emailme@isaacg.net>
2021-07-07 21:09:48 -0500

    ⚡saving prog

--- lesson_6/tictactoe/overview.js
+++ lesson_6/tictactoe/overview.js
@@ -1,6 +1,23 @@
 /*
     Decomposing the Problem: 
     Q: Before we start coding what must we do first, when first introduced with a new problem? 
+    - A: I must really understand the problem that I am being 
+         asked to solve. 
+
+         I must understand the problem, then design a mental model of the solution sometimes in the form of a flowchart.
+
+         But in order to accomplish this I must work backwards to break down the problem to several sub-problems. A good first step is to describe the problem in your own words, then move on to construct a high-level flow chart. 
+
+    Q: Describe the game of tic-tac-toe: 
+    - Tic-Tac-Toe
+         2 player-game
+         Played on a 3 x 3 grid called the board
+         Each player take a turn marking there square on the board
+         First player to get three 
+
+
+
+
 
     Q: What are the rule so of tic-tac-toe?
 
