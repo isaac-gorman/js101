4c552f7ee78ae37182608c692721055c96bd68ba
Isaac Gorman <emailme@isaacg.net>
2021-07-07 21:01:57 -0500

    ⚡saving prog

--- lesson_6/tictactoe/overview.js
+++ lesson_6/tictactoe/overview.js
@@ -1,1 +1,20 @@
- 
+/*
+    Decomposing the Problem: 
+    Q: Before we start coding what must we do first, when first introduced with a new problem? 
+
+    Q: What are the rule so of tic-tac-toe?
+
+    Q: What is the generic sequence of the game? 
+
+    Q: What do the main two loops do with the function do? 
+
+    Q: What is crucial when breaking down a problem breaking down the problem initially? 
+
+    Flowchart: 
+
+    construct you flowchart for the game
+    
+
+
+
+*/
