2e63d62f5ae4f8d31fd8a2091d358268d57a5983
Isaac Gorman <emailme@isaacg.net>
2021-07-09 15:26:19 -0500

    ⚡saving prog

--- lesson_6/tictactoe/My_Solution/notes.md
+++ lesson_6/tictactoe/My_Solution/notes.md
@@ -4,3 +4,4 @@
 - Display the choice board that is linked to the actual board
 - Whenever a new mark is added by either play the choice is eliminated form the choice bar
 - Display the updated game board
+- Computer marks a square
--- lesson_6/tictactoe/My_Solution/step2.js
+++ lesson_6/tictactoe/My_Solution/step2.js
@@ -3,18 +3,18 @@ const READ_LINE = require('readline-sync');
 let displayBoard = (board) => console.table(board);
 let initializeBoard = () => {
   let gameBoard = [];
-  gameBoard['row_one'] = [[], [], []];
-  gameBoard['row_two'] = [[], [], []];
-  gameBoard['row_three'] = [[], [], []];
+  gameBoard[0] = [[], [], []];
+  gameBoard[1] = [[], [], []];
+  gameBoard[3] = [[], [], []];
 
   return gameBoard;
 };
 
 let choiceBoard = () => {
   let choiceBoard = [];
-  choiceBoard['row_one'] = [['R1-C1'], ['R1-C2'], ['R1-C3']];
-  choiceBoard['row_two'] = [['R2-C1'], ['R2-C2'], ['R2-C3']];
-  choiceBoard['row_three'] = [['R3-C1'], ['R3-C2'], ['R3-C3']];
+  choiceBoard[0] = [['R1-C1'], ['R1-C2'], ['R1-C3']];
+  choiceBoard[1] = [['R2-C1'], ['R2-C2'], ['R2-C3']];
+  choiceBoard[2] = [['R3-C1'], ['R3-C2'], ['R3-C3']];
 
   return choiceBoard;
 };
@@ -31,46 +31,59 @@ let playerChoice = READ_LINE.question(displayBoard(optionsBoard));
 switch (playerChoice) {
   // cases for first-row
   case 'R1-C1':
-    board['row_one'][0] = 'X';
-    optionsBoard['row_one'][0] = 'X';
+    board[0][0] = 'X';
+    optionsBoard[0][0] = 'X';
     break;
   case 'R1-C2':
-    board['row_one'][1] = 'X';
-    optionsBoard['row_one'][1] = 'X';
+    board[0][1] = 'X';
+    optionsBoard[0][1] = 'X';
     break;
   case 'R1-C3':
-    board['row_one'][2] = 'X';
-    optionsBoard['row_one'][2] = 'X';
+    board[0][2] = 'X';
+    optionsBoard[0][2] = 'X';
     break;
 
   // cases for second-row
   case 'R2-C1':
-    board['row_two'][0] = 'X';
-    optionsBoard['row_two'][0] = 'X';
+    board[1][0] = 'X';
+    optionsBoard[1][0] = 'X';
     break;
   case 'R2-C2':
-    board['row_two'][1] = 'X';
-    optionsBoard['row_two'][1] = 'X';
+    board[1][1] = 'X';
+    optionsBoard[1][1] = 'X';
     break;
   case 'R2-C3':
-    board['row_two'][2] = 'X';
-    optionsBoard['row_two'][2] = 'X';
+    board[1][2] = 'X';
+    optionsBoard[1][2] = 'X';
     break;
 
   // cases for third-row
   case 'R3-C1':
-    board['row_three'][0] = 'X';
-    optionsBoard['row_three'][0] = 'X';
+    board[2][0] = 'X';
+    optionsBoard[2][0] = 'X';
     break;
   case 'R3-C2':
-    board['row_three'][1] = 'X';
-    optionsBoard['row_three'][1] = 'X';
+    board[2][1] = 'X';
+    optionsBoard[2][1] = 'X';
     break;
   case 'R3-C3':
-    board['row_three'][2] = 'X';
-    optionsBoard['row_three'][2] = 'X';
+    board[2][2] = 'X';
+    optionsBoard[2][2] = 'X';
     break;
 
   default:
 }
 displayBoard(board);
+
+// based on the current available boxes the computer will pick at random a choice
+// - choice can be based on random numbers within the range of the current choices within the board.
+
+let computerChoice = () => {
+  console.log(optionsBoard);
+  let randomRange = optionsBoard.map((subArray) =>
+    console.log('sub-array', subArray)
+  );
+  return randomRange;
+};
+
+computerChoice();
