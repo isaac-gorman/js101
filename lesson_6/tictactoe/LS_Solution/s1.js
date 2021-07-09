7b9b5e3d0fcb852c77cde1ff7106c87c08a4c810
Isaac Gorman <emailme@isaacg.net>
2021-07-09 14:08:57 -0500

    ⚡saving prog

---
+++ lesson_6/tictactoe/LS_Solution/s1.js
@@ -0,0 +1,30 @@
+function displayBoard(board) {
+  console.log("");
+  console.log("     |     |");
+  console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["4"]}  |  ${board["5"]}  |  ${board["6"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["7"]}  |  ${board["8"]}  |  ${board["9"]}`);
+  console.log("     |     |");
+  console.log("");
+}
+
+function initializeBoard() {
+  // empty object
+  let initialBoard = {};
+
+  // use a for loop to construct each level of the board
+  for (let square = 1; square <= 9; square++) {
+    initialBoard[String(square)] = " ";
+  }
+
+  return initialBoard;
+}
+
+let board = initializeBoard();
+displayBoard(board);
