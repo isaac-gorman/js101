6756598ba5f13340076cc2dd52914d459f18f0e6
Isaac Gorman <emailme@isaacg.net>
2021-07-13 14:25:35 -0500

    ⚡prog

--- lesson_6/tictactoe/LS_Solution/computerAI.js
+++ lesson_6/tictactoe/LS_Solution/computerAI.js
@@ -61,9 +61,12 @@ function checkMoves(gameBoard) {
     console.log("sq1: ", sq1);
     console.log("sq2: ", sq2);
 
+    console.log("gameBoard[sq1]: ", gameBoard[sq1]);
+    console.log("gameBoard[sq2]: ", gameBoard[sq2]);
+
     if (gameBoard[sq1] === HUMAN_MARKER && gameBoard[sq2] === HUMAN_MARKER) {
       console.log("true");
-      output = { result: true, counterMove: potentialWins[2][0] };
+      return (output = { result: true, counterMove: potentialWins[2][0] });
     } else {
       console.log("false");
       output = { result: false };
